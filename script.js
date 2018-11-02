21398b7d763abef9289be6e475c1657ff4cbb458
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-02 09:56:31 +0100

    Create menu by render function

--- script.js
+++ script.js
@@ -7,8 +7,16 @@ function SideBarMenu () {
  }
  
  SideBarMenu.prototype.renderFunction = function() {
-     
+     const menuDiv = document.createElement('div')
+
+     // Add styles here!
+
+     document.body.appendChild(menuDiv)
+     menuDiv.style.backgroundColor = this.bgColor
+     menuDiv.style.height = '300px'
  };
  
  
- const menu1 = new SideBarMenu();
+ const menu1 = new SideBarMenu()
+
+ menu1.renderFunction()
