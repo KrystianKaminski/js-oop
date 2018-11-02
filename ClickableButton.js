4b4a70c4a90bbea49c671459e4ec80cecd76aa76
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-02 11:50:18 +0100

    Refactoring code

--- ClickableButton.js
+++ ClickableButton.js
@@ -1,27 +1,13 @@
 function ClickableButton(label, handler) {
     this.label = label
     this.handler = handler
-
     this.renderFunction()
 }
 
 
 ClickableButton.prototype.renderFunction = function () {
-    this.menuButton = document.createElement('button')
-
-    this.label = document.createTextNode(`${this.label}`)
-
-    this.menuButton.style.top = '200px'
-    this.menuButton.style.left = '200px'
-    this.menuButton.style.height = '200px'
-    this.menuButton.style.width = '400px'
-    this.menuButton.style.backgroundColor = 'red'
-
-    this.menuButton.addEventListener('click', () => {
-        this.handler (this.handler)
-    })
-
-    this.menuButton.appendChild(this.label)
-
-    document.body.appendChild(this.menuButton)
+    const button = document.createElement('button')
+    button.innerText = this.label
+    button.addEventListener('click', this.handler)
+    document.body.appendChild(button);
 }
