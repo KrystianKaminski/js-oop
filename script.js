da1f6da4c393dd5575c980c286142f4fc500192c
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-02 11:02:08 +0100

    Toggling menu functionality

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function SideBarMenu (color, direction) {
     this.isOnTheLeft = direction === 'left' ? true : false
     this.isOnTheRight = direction === 'right' ? true : false
     this.bgColor = color || 'red'
+    this.menuDiv = null
     if (!this.isOnTheLeft && !this.isOnTheRight) {
         this.isOnTheLeft = true
     }
@@ -11,27 +12,38 @@ function SideBarMenu (color, direction) {
  }
  
  SideBarMenu.prototype.renderFunction = function() {
-     const menuDiv = document.createElement('div')
+     if (this.menuDiv) this.menuDiv.remove()
+     this.menuDiv = document.createElement('div')
 
      // Add styles here!
 
-     document.body.appendChild(menuDiv)
-     menuDiv.style.backgroundColor = this.bgColor
-     menuDiv.style.height = '100vh'
-     menuDiv.style.width = '200px'
-     menuDiv.style.position = 'fixed'
-     menuDiv.style.top = '0'
+     document.body.appendChild(this.menuDiv)
+     this.menuDiv.style.backgroundColor = this.bgColor
+     this.menuDiv.style.height = '100vh'
+     this.menuDiv.style.width = '200px'
+     this.menuDiv.style.position = 'fixed'
+     this.menuDiv.style.top = '0'
     
-     if (this.isOnTheLeft) menuDiv.style.left = '0'
-     if (this.isOnTheRight) menuDiv.style.right = '0'
-     if (!this.isOpen) menuDiv.style.display = 'none'
+     if (this.isOnTheLeft) this.menuDiv.style.left = '0'
+     if (this.isOnTheRight) this.menuDiv.style.right = '0'
+     if (!this.isOpen) this.menuDiv.style.display = 'none'
 
  };
 
  SideBarMenu.prototype.init = function() {
      this.renderFunction()
  }
+
+ SideBarMenu.prototype.toggleMenu = function() {
+    if (this.isOpen === true) {
+        this.isOpen = false
+    } else {
+        this.isOpen = true
+    }
+    this.renderFunction()
+}
  
  
  const menu1 = new SideBarMenu()
- const menu2 = new SideBarMenu('green', 'right');
+ const menu2 = new SideBarMenu('green', 'right')
+ menu2.toggleMenu()
