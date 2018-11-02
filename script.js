029482b8229e785fc9d48207d1364373a733d2c5
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-02 09:48:27 +0100

    Initial

---
+++ script.js
@@ -0,0 +1,14 @@
+function SideBarMenu () {
+    this.isOpen = true
+    this.isOnTheLeft = true
+    this.isOnTheRight = false
+    this.bgColor = 'red'
+ 
+ }
+ 
+ SideBarMenu.prototype.renderFunction = function() {
+     
+ };
+ 
+ 
+ const menu1 = new SideBarMenu();
