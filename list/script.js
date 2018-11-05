c71f936e1d491e114b5a4cf89d5536b49fff29e7
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-05 11:18:20 +0100

    Add input area and adding functionality

--- list/script.js
+++ list/script.js
@@ -1,35 +1,46 @@
 function ToDo() {
-    this.tasks = []
+  this.tasks = [];
+
+  this.render();
 }
 
 function Task(text) {
-    this.text = text
-}
+  this.text = text;
 
+}
 
 ToDo.prototype.addTask = function(text) {
-    this.tasks.push(new Task(text))
-}
+  this.tasks.push(new Task(text));
+  this.render();
+};
 
 ToDo.prototype.render = function() {
-    document.body.innerHTML = ''
-
+    document.body.innerHTML = "";
+    this.makeUI()
+    const ul = document.createElement("ul");
     this.tasks.forEach(task => {
-        const div = document.createElement('div')
-        div.innerText = task.text
-        document.body.appendChild(div)
-    })
+        const li = document.createElement("li");
+        li.innerText = task.text;
+        ul.appendChild(li);
+    });
+    document.body.appendChild(ul);
+};
+
+ToDo.prototype.makeUI = function() {
+    const input = document.createElement('input')
+    const button = document.createElement('button')
+
+    button.innerText = 'Click here'
+        button.addEventListener('click', () => {
+            this.addTask(input.value)
+        })
+
+    document.body.appendChild(input)
+    document.body.appendChild(button)
 }
 
-const a = new ToDo()
+const a = new ToDo();
 
-a.addTask('text')
-a.addTask('text')
-a.addTask('text')
-a.addTask('text')
-a.addTask('text')
-a.addTask('text')
-a.addTask('text')
-a.addTask('text')
-
-a.render()
+// a.addTask("text");
+// a.addTask("text");
+// a.addTask("text");
