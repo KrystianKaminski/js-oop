99e01d7489d3685b2ae8f2d671653e49318360b5
Krystian Kaminski <krystiankaminski07@gmail.com>
2018-11-05 12:41:07 +0100

    Add deleting items functionality

--- list/script.js
+++ list/script.js
@@ -1,4 +1,3 @@
-
 class ToDo {
     constructor() {
         this.tasks = []
@@ -6,32 +5,43 @@ class ToDo {
     }
 
     addTask(text) {
-        this.tasks.push(new Task(text));
+        this.tasks.push(new Task(text))
         this.render()
       }
     render() {
-        document.body.innerHTML = "";
+        document.body.innerHTML = ""
         this.makeUI()
-        const ul = document.createElement("ul");
+        const ul = document.createElement("ul")
         this.tasks.forEach(task => {
-            const li = document.createElement("li");
-            li.innerText = task.text;
-            ul.appendChild(li);
+            const li = document.createElement("li")
+            li.innerText = task.text
+            ul.appendChild(li)
         })
-        document.body.appendChild(ul);
+        document.body.appendChild(ul)
+    }
+    deleteAll() {
+        document.body.innerHTML = ""
+        this.tasks = []
+        this.makeUI()
     }
 
     makeUI() {
         const input = document.createElement('input')
         const button = document.createElement('button')
+        const button1 = document.createElement('button')
 
         button.innerText = 'Click here'
+        button1.innerText = 'Delete all'
             button.addEventListener('click', () => {
                 this.addTask(input.value)
+            })
+            button1.addEventListener('click', () => {
+                this.deleteAll()
             })
 
         document.body.appendChild(input)
         document.body.appendChild(button)
+        document.body.appendChild(button1)
     }
 
 }
